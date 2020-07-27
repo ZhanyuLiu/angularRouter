329a68c8801dbb26c2460618c38a556b5b1554cb
ZhanyuLiu <1635259245@qq.com>
2020-07-27 10:46:40 +0800

    路由挂载

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+// 配置路由模块
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { NewsComponent } from './components/news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home', component:HomeComponent
+  },
+  {
+    path:'news', component:NewsComponent
+  },
+  {
+    path:'product', component:ProductComponent
+  },
+  {
+    path:'**', redirectTo:'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductComponent } from './components/product/product.component';
+import { NewsComponent } from './components/news/news.component';
+import { NewscontentComponent } from './components/newscontent/newscontent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductComponent,
+    NewsComponent,
+    NewscontentComponent
   ],
   imports: [
     BrowserModule,
