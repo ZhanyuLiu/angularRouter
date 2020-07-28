04e2f2714e012ccf0cc5675b64a556ccdae29d28
ZhanyuLiu <1635259245@qq.com>
2020-07-28 11:43:22 +0800

    路由嵌套 父子路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,9 +7,25 @@ import { NewsComponent } from './components/news/news.component';
 import { NewscontentComponent } from './components/newscontent/newscontent.component';
 import { ProductcontentComponent } from './components/productcontent/productcontent.component';
 
+import { WelcomeComponent } from './components/home/welcome/welcome.component';
+import { SettingComponent } from './components/home/setting/setting.component';
+import { PcateComponent } from './components/product/pcate/pcate.component';
+import { PlistComponent } from './components/product/plist/plist.component';
+
 const routes: Routes = [
   {
-    path:'home', component:HomeComponent
+    path:'home', component:HomeComponent,
+    children:[
+      {
+        path:'welcome', component:WelcomeComponent
+      },
+      {
+        path:'setting', component:SettingComponent
+      },
+      {
+        path:'**', redirectTo:'welcome'
+      }
+    ]
   },
   {
     path:'news', component:NewsComponent
@@ -20,8 +36,20 @@ const routes: Routes = [
     // // 动态传值
     path:'newscontent/:aid', component:NewscontentComponent
   },
+
   {
-    path:'product', component:ProductComponent
+    path:'product', component:ProductComponent,
+    children:[
+      {
+        path:'pcate', component:PcateComponent
+      },
+      {
+        path:'plist', component:PlistComponent
+      },
+      {
+        path:'**', redirectTo:'pcate'
+      }
+    ]
   },
   {
     path:'productcontent/:pid', component:ProductcontentComponent
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { ProductComponent } from './components/product/product.component';
 import { NewsComponent } from './components/news/news.component';
 import { NewscontentComponent } from './components/newscontent/newscontent.component';
 import { ProductcontentComponent } from './components/productcontent/productcontent.component';
+import { WelcomeComponent } from './components/home/welcome/welcome.component';
+import { SettingComponent } from './components/home/setting/setting.component';
+import { PcateComponent } from './components/product/pcate/pcate.component';
+import { PlistComponent } from './components/product/plist/plist.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { ProductcontentComponent } from './components/productcontent/productcont
     ProductComponent,
     NewsComponent,
     NewscontentComponent,
-    ProductcontentComponent
+    ProductcontentComponent,
+    WelcomeComponent,
+    SettingComponent,
+    PcateComponent,
+    PlistComponent
   ],
   imports: [
     BrowserModule,
